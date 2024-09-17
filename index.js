4cde7e0eb480f3a21a18ed6addc405308c9fe6d7
Cameron Graham <36056681+thetubbydoughnut@users.noreply.github.com>
2024-09-17 12:13:32 -0500

    made the server run

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // require your server and launch it here
+
+const server = require('./api/server')
+
+const port = 5000
+
+server.listen(port, () => {
+    console.log(`Server Running on http://localhost:${port}`)
+})
