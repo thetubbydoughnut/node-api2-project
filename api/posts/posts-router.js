d3c61f49f7da88c1721afa74dffa685447cd09a9
Cameron Graham <36056681+thetubbydoughnut@users.noreply.github.com>
2024-09-17 12:49:38 -0500

    2nd endpoint is done

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -13,7 +13,24 @@ router.get('/', async (req, res) => {
         console.error(err)
         res.status(500).json({ message: "The posts information could not be retrieved" })
     }
-})
+});
+
+router.get('/:id', async (req, res) => {
+    try {
+        const { id } = req.params
+        const postsById = await Post.findById(id);
+        if (!postsById) {
+            res.status(404).json({ message: "The post with the specified ID does not exist" })
+        }
+        else {
+            res.status(200).json(postsById)
+        }
+    }
+    catch (err) {
+        console.error(err)
+        res.status(500).json({ message: "The post information could not be retrieved" })
+    }
+});
 
 
 
